1ffce6f89a0440c12b67aea6cbffb750879b79dc
Ben Cordero <bencord0@condi.me>
2022-11-04 09:59:25 +0000

    orb: Address failing linters

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -4,9 +4,9 @@ TARGETS=(
     ./target/release/hello
 )
 
-for target in ${TARGETS[@]}; do
+for target in "${TARGETS[@]}"; do
     strip -v "${target}"
 done
 
-tar cvz ${TARGETS[@]} | base64 | cat ./scripts/unpack.sh.in - > ./src/scripts/unpack.sh
+tar cvz "${TARGETS[@]}" | base64 | cat ./scripts/unpack.sh.in - > ./src/scripts/unpack.sh
 chmod -v +x ./src/scripts/unpack.sh
