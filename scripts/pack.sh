8117b94501a4097173f5087883a3a3518440a7e8
Ben Cordero <bencord0@condi.me>
2022-11-04 10:46:55 +0000

    scripts/pack: ensure output directory exists

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -8,5 +8,6 @@ for target in "${TARGETS[@]}"; do
     strip -v "${target}"
 done
 
+mkdir -pv ./src/workspace/scripts
 tar cvz "${TARGETS[@]}" | base64 | cat ./scripts/unpack.sh.in - > ./src/workspace/scripts/unpack.sh
 chmod -v +x ./src/workspace/scripts/unpack.sh
